79ccd2eb8114f17862a7283746720ba37cd07c1a
UniverseStark <jty90k@gmail.com>
2021-03-15 15:30:11 +0900

    Titles and Subtitles and fix the App.js

--- App.js
+++ App.js
@@ -33,7 +33,6 @@ export default class extends React.Component {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("We can not find you", "So sad");
     }
